7dc25ecd68ad8b592ed4f352d19f105e9a300254
Piotr Ponikowski <piopon.github@gmail.com>
2024-10-02 23:26:44 +0200

    updated source code to separate view (only layout) from controller (ctrl logic)

--- src/main/java/codes/pnk/HelloController.java
+++ src/main/java/codes/pnk/HelloController.java
@@ -1,14 +1,30 @@
 package codes.pnk;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
-public class HelloController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelloController implements Initializable {
     @FXML
     private Label welcomeText;
-
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button button;
+    private Stage stage;
+
+    public HelloController(Stage stage) {
+        this.stage = stage;
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        welcomeText.setText("No file selected");
+        button.setOnAction(actionEvent -> {
+            welcomeText.setText("Changed!");
+        });
     }
 }
