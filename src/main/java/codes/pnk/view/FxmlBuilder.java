78373afed70f1a778b174f8a835234b5d02f877c
Piotr Ponikowski <piopon.github@gmail.com>
2024-10-05 19:53:00 +0200

    added new FXML builder class responsible for invoking the layout build command

---
+++ src/main/java/codes/pnk/view/FxmlBuilder.java
@@ -0,0 +1,26 @@
+package codes.pnk.view;
+
+import codes.pnk.Main;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Region;
+import javafx.stage.Stage;
+import javafx.util.Builder;
+
+import java.io.IOException;
+
+public class FxmlBuilder  implements Builder<Region> {
+    private final Stage stage;
+
+    public FxmlBuilder(Stage stage) {
+        this.stage = stage;
+    }
+
+    @Override
+    public Region build() {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("view.fxml"));
+        FxmlController controller = new FxmlController(stage);
+        fxmlLoader.setController(controller);
+        return null;
+    }
+}
