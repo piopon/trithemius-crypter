270298e8b08e4e585478d834d28e657f1ac42c17
Piotr Ponikowski <piopon.github@gmail.com>
2024-10-05 19:54:05 +0200

    added proper view controller logic to return view Region

--- src/main/java/codes/pnk/controller/ViewController.java
+++ src/main/java/codes/pnk/controller/ViewController.java
@@ -1,4 +1,17 @@
 package codes.pnk.controller;
 
+import codes.pnk.view.FxmlBuilder;
+import javafx.scene.layout.Region;
+import javafx.stage.Stage;
+
 public class ViewController {
+    private final FxmlBuilder viewBuilder;
+
+    public ViewController(Stage stage) {
+        this.viewBuilder = new FxmlBuilder(stage);
+    }
+
+    public Region getView() {
+        return viewBuilder.build();
+    }
 }
