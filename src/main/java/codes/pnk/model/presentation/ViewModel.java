cfac69429066784ea71a48e76ae8f941aee0ff96
Piotr Ponikowski <piopon.github@gmail.com>
2024-10-06 23:18:10 +0200

    updated ViewModel from record to class (to make it mutable)

--- src/main/java/codes/pnk/model/presentation/ViewModel.java
+++ src/main/java/codes/pnk/model/presentation/ViewModel.java
@@ -2,10 +2,67 @@ package codes.pnk.model.presentation;
 
 import java.io.File;
 import java.nio.file.Path;
+import java.util.Objects;
 import java.util.Optional;
 
-public record ViewModel(Optional<File> imageFile, Optional<File> textFile, Optional<Path> outputPath) {
+public final class ViewModel {
+    private Optional<File> imageFile;
+    private Optional<File> textFile;
+    private Optional<Path> outputPath;
+
+    public ViewModel(Optional<File> imageFile, Optional<File> textFile, Optional<Path> outputPath) {
+        this.imageFile = imageFile;
+        this.textFile = textFile;
+        this.outputPath = outputPath;
+    }
+
     public ViewModel() {
         this(Optional.empty(), Optional.empty(), Optional.empty());
+    }
+
+    public Optional<File> getImageFile() {
+        return imageFile;
+    }
+
+    public void setImageFile(Optional<File> imageFile) {
+        this.imageFile = imageFile;
+    }
+
+    public Optional<File> getTextFile() {
+        return textFile;
+    }
+
+    public void setTextFile(Optional<File> textFile) {
+        this.textFile = textFile;
+    }
+
+    public Optional<Path> getOutputPath() {
+        return outputPath;
+    }
+
+    public void setOutputPath(Optional<Path> outputPath) {
+        this.outputPath = outputPath;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this)
+            return true;
+        if (obj == null || obj.getClass() != this.getClass())
+            return false;
+        var that = (ViewModel) obj;
+        return Objects.equals(this.imageFile, that.imageFile) && Objects.equals(this.textFile,
+                                                                                that.textFile) && Objects.equals(
+                this.outputPath, that.outputPath);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(imageFile, textFile, outputPath);
+    }
+
+    @Override
+    public String toString() {
+        return "ViewModel[" + "imageFile=" + imageFile + ", " + "textFile=" + textFile + ", " + "outputPath=" + outputPath + ']';
     }
 }
