098d13f312bda5dc175ce308f0b3e937853e412e
Piotr Ponikowski <piopon.github@gmail.com>
2024-10-18 18:43:46 +0200

    marked image class fields as final + updated byte[] ctor fields

--- src/main/java/codes/pnk/model/domain/common/Image.java
+++ src/main/java/codes/pnk/model/domain/common/Image.java
@@ -4,9 +4,9 @@ import javax.imageio.metadata.IIOMetadata;
 import java.awt.image.BufferedImage;
 
 public class Image {
-    private String name;
-    private BufferedImage data;
-    private IIOMetadata metadata;
+    private final String name;
+    private final BufferedImage data;
+    private final IIOMetadata metadata;
 
     public Image(final String name, final BufferedImage data, final IIOMetadata metadata) {
         this.name = name;
@@ -16,5 +16,7 @@ public class Image {
 
     public Image(final String name, final byte[] data) {
         this.name = name;
+        this.data = null;
+        this.metadata = null;
     }
 }
