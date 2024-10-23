20e5d5d80aced243c61555ab6df5d5fa6c353997
Piotr Ponikowski <piopon.github@gmail.com>
2024-10-23 20:25:48 +0200

    added logic to read input stream data to bytes array

--- src/main/java/codes/pnk/model/domain/common/Image.java
+++ src/main/java/codes/pnk/model/domain/common/Image.java
@@ -1,8 +1,11 @@
 package codes.pnk.model.domain.common;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.IOException;
 
 public class Image {
+    private static final int BUFFER_SIZE = 512;
 
     private final File sourceFile;
     private final byte[] data;
@@ -11,5 +14,16 @@ public class Image {
         this.sourceFile = file;
     }
 
+    private byte[] fileStreamToBytes(InputStream is) throws ImageException {
+        try (ByteArrayOutputStream stream = new ByteArrayOutputStream()) {
+            int bytesRead;
+            byte[] data = new byte[BUFFER_SIZE];
+            while ((bytesRead = is.read(data, 0, BUFFER_SIZE)) >= 0) {
+                stream.write(data, 0, bytesRead);
+            }
+            return stream.toByteArray();
+        } catch (IOException e) {
+            throw new ImageException(e);
+        }
     }
 }
