3ece25889d72a9fc2197002b80a3ccddbf982363
Piotr Ponikowski <piopon.github@gmail.com>
2024-10-25 22:18:25 +0200

    added the buffered output image field to LSB algorithm stream

--- src/main/java/codes/pnk/model/domain/algorithm/dh/lsb/LsbOutputStream.java
+++ src/main/java/codes/pnk/model/domain/algorithm/dh/lsb/LsbOutputStream.java
@@ -4,20 +4,35 @@ import codes.pnk.model.domain.common.Image;
 import codes.pnk.model.domain.common.ImageException;
 import codes.pnk.model.domain.common.Text;
 
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.OutputStream;
 
 public class LsbOutputStream extends OutputStream {
     private final Image image;
     private final Text text;
+    private BufferedImage output;
 
     public LsbOutputStream(final Image image, final Text text) throws ImageException {
         this.image = image;
         this.text = text;
+        this.output = initializeOutputImage();
     }
 
     @Override
     public void write(int b) throws IOException {
         return;
+    }
+
+    private BufferedImage initializeOutputImage() throws ImageException {
+        int imageWidth = this.image.getBufferedImageData().getWidth();
+        int imageHeight = this.image.getBufferedImageData().getHeight();
+        BufferedImage newImg = new BufferedImage(imageWidth, imageHeight, BufferedImage.TYPE_INT_RGB);
+        for (int x = 0; x < imageWidth; x++) {
+            for (int y = 0; y < imageHeight; y++) {
+                newImg.setRGB(x, y, this.image.getBufferedImageData().getRGB(x, y));
+            }
+        }
+        return newImg;
     }
 }
