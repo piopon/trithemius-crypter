a5f5d4c3f6281323411b88853d97ef1d046b926c
Piotr Ponikowski <piopon.github@gmail.com>
2024-10-27 11:58:13 +0100

    added method to convert Text file input to raw bytes data

--- src/main/java/codes/pnk/model/domain/common/Text.java
+++ src/main/java/codes/pnk/model/domain/common/Text.java
@@ -1,8 +1,10 @@
 package codes.pnk.model.domain.common;
 
-import java.io.File;
+import java.io.*;
 
 public class Text {
+    private static final int BUFFER_SIZE = 512;
+
     private final File sourceFile;
 
     public Text(final File file) {
@@ -15,5 +17,16 @@ public class Text {
 
     public byte[] getData() {
         return null;
+    }
+
+    private byte[] streamToBytes(InputStream is) throws IOException {
+        try (ByteArrayOutputStream stream = new ByteArrayOutputStream()) {
+            int bytesRead;
+            byte[] data = new byte[BUFFER_SIZE];
+            while ((bytesRead = is.read(data, 0, BUFFER_SIZE)) >= 0) {
+                stream.write(data, 0, bytesRead);
+            }
+            return stream.toByteArray();
+        }
     }
 }
