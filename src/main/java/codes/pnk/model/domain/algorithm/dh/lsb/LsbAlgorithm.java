7108e267ff5c20a0b8cc64eaed7b8da0492c9b5f
Piotr Ponikowski <piopon.github@gmail.com>
2024-10-27 12:11:18 +0100

    restored LSB data hiding alogirthm code in new dev branch

---
+++ src/main/java/codes/pnk/model/domain/algorithm/dh/lsb/LsbAlgorithm.java
@@ -0,0 +1,32 @@
+package codes.pnk.model.domain.algorithm.dh.lsb;
+
+import codes.pnk.model.domain.algorithm.Algorithm;
+import codes.pnk.model.domain.common.Image;
+import codes.pnk.model.domain.exception.AlgorithmException;
+import codes.pnk.model.domain.exception.ImageException;
+import codes.pnk.model.domain.common.Text;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+public class LsbAlgorithm extends Algorithm {
+    @Override
+    public byte[] embed(Text inText, Image inImage) throws AlgorithmException {
+        try {
+            BufferedImage outputImage = null;
+            try (LsbOutputStream lsbOS = new LsbOutputStream(inImage, inText)) {
+                lsbOS.write(inText.getData());
+                lsbOS.flush();
+                outputImage = lsbOS.getOutput();
+            }
+            return new Image(inImage.getPath(), outputImage).getRawImageData();
+        } catch (IOException | ImageException e) {
+            throw new AlgorithmException(e);
+        }
+    }
+
+    @Override
+    public byte[] extract(Image inImage) throws AlgorithmException {
+        return new byte[0];
+    }
+}
