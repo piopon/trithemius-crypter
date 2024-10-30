9f145b24adf8b98e51ab6524fef4d0bafc111e2b
Piotr Ponikowski <piopon.github@gmail.com>
2024-10-30 23:01:30 +0100

    added logic to retrieve Image object source file name value

--- src/main/java/codes/pnk/model/domain/common/Image.java
+++ src/main/java/codes/pnk/model/domain/common/Image.java
@@ -31,6 +31,10 @@ public class Image {
         this.data = bufferedImageToData(bufferedImage);
     }
 
+    public String getName() {
+        return sourceFile.getName();
+    }
+
     public String getPath() {
         return sourceFile.getAbsolutePath();
     }
