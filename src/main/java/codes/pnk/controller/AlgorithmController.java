f5be57804bdb25b07fe0e8008d6b334a15a09415
Piotr Ponikowski <piopon.github@gmail.com>
2024-10-30 23:38:17 +0100

    added new AlgorithmController class responsible for invoking steganography action

---
+++ src/main/java/codes/pnk/controller/AlgorithmController.java
@@ -0,0 +1,11 @@
+package codes.pnk.controller;
+
+import codes.pnk.model.domain.algorithm.Algorithm;
+
+public class AlgorithmController {
+    private final Algorithm algorithm;
+
+    public AlgorithmController(final Algorithm algorithm) {
+        this.algorithm = algorithm;
+    }
+}
