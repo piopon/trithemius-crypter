cd688ca93fd1aa02b6ae8a3f25dea45448cdec1f
Piotr Ponikowski <piopon.github@gmail.com>
2024-11-06 18:55:45 +0100

    removed not needed import statements in previously changed files

--- src/main/java/codes/pnk/view/FxmlController.java
+++ src/main/java/codes/pnk/view/FxmlController.java
@@ -3,22 +3,15 @@ package codes.pnk.view;
 import codes.pnk.Main;
 import codes.pnk.model.presentation.ViewConfig;
 import codes.pnk.model.presentation.ViewModel;
-import javafx.beans.property.ObjectProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
 import javafx.scene.control.TabPane;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
-import javafx.stage.DirectoryChooser;
-import javafx.stage.FileChooser;
 
-import java.io.File;
 import java.io.IOException;
 import java.net.URL;
-import java.nio.file.Path;
 import java.util.Map;
 import java.util.ResourceBundle;
 
