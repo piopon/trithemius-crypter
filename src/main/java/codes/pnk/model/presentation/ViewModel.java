7b623c290de740236b6c5e79b8789d4df91c0a3e
Piotr Ponikowski <piopon.github@gmail.com>
2024-11-10 21:22:45 +0100

    changed view model to be of record type

--- src/main/java/codes/pnk/model/presentation/ViewModel.java
+++ src/main/java/codes/pnk/model/presentation/ViewModel.java
@@ -7,49 +7,6 @@ import java.io.File;
 import java.nio.file.Path;
 import java.util.Objects;
 
-public final class ViewModel {
-    private final ObjectProperty<File> imageFile;
-    private final ObjectProperty<File> textFile;
-    private final ObjectProperty<Path> outputPath;
-
-    public ViewModel() {
-        this(null, null, null);
-    }
-
-    public ViewModel(final File imageFile, final File textFile, final Path outputPath) {
-        this.imageFile = new SimpleObjectProperty<>(imageFile);
-        this.textFile = new SimpleObjectProperty<>(textFile);
-        this.outputPath = new SimpleObjectProperty<>(outputPath);
-    }
-
-    public ObjectProperty<File> getImageFile() {
-        return imageFile;
-    }
-
-    public ObjectProperty<File> getTextFile() {
-        return textFile;
-    }
-
-    public ObjectProperty<Path> getOutputPath() {
-        return outputPath;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == this) return true;
-        if (obj == null || obj.getClass() != this.getClass()) return false;
-        var that = (ViewModel) obj;
-        return Objects.equals(this.imageFile, that.imageFile) && Objects.equals(this.textFile, that.textFile)
-               && Objects.equals(this.outputPath, that.outputPath);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(imageFile, textFile, outputPath);
-    }
-
-    @Override
-    public String toString() {
-        return "ViewModel[imageFile=" + imageFile + ", textFile=" + textFile + ", outputPath=" + outputPath + ']';
-    }
+public record ViewModel(ObjectProperty<File> imageFile, ObjectProperty<File> textFile,
+                        ObjectProperty<Path> outputPath) {
 }
